e2a7a3ef36da68a92643062e4c90b47505b54e89
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-04-28 18:05:16 -0700

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30, case_sensitive: false }
 end
