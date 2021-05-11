e85bb52499748996394bd0107a26af2283cb46d7
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-05-11 11:45:11 -0700

    code review

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @created_events = current_user.privates
-    @past_events = current_user.attending_events.past
-    @upcoming_events = current_user.attending_events.upcoming
+    @user_past_events = current_user.attending_events.past
+    @user_upcoming_events = current_user.attending_events.upcoming
+    @past_events = Private.past
+    @future_events = Private.upcoming
   end
 end
-# @user_attended_events = @current_user.attending_events.past
